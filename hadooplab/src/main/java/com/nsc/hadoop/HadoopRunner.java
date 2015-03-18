4bacd0f1c8eba54d65eba043078672b552e48356
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-03-19 00:44:39 +0700

    add maven for hadoop

--- hadooplab/src/main/java/com/nsc/hadoop/HadoopRunner.java
+++ hadooplab/src/main/java/com/nsc/hadoop/HadoopRunner.java
@@ -1,8 +1,78 @@
 package com.nsc.hadoop;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class HadoopRunner {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			List<String> lst = new ArrayList<>();
+			while (itr.hasMoreTokens()) {
+				String str = itr.nextToken();
+				System.out.println(str);
+				lst.add(str);
+				word.set(str);
+				context.write(word, one);
+			}
+			System.out.println(String.join(",", lst));
+		}
 	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) {
+		Configuration conf = new Configuration();
+		try {
+			Job job = new Job(conf, "word count");
+			job.setJarByClass(HadoopRunner.class);
+			job.setMapperClass(TokenizerMapper.class);
+			job.setCombinerClass(IntSumReducer.class);
+			job.setReducerClass(IntSumReducer.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(IntSumReducer.class);
+		    FileInputFormat.addInputPath(job, new Path(args[0]));
+		    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+			System.exit(job.waitForCompletion(true) ? 0 : 1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
